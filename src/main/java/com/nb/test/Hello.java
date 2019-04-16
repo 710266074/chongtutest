ab8f77d4e03b723dac9aa8938e7676007ed07d05
710266074 <710266074@qq.com>
2019-04-16 21:14:15 +0800

    初始化

---
+++ src/main/java/com/nb/test/Hello.java
@@ -0,0 +1,13 @@
+package com.nb.test;
+
+/**
+ * @author lpq
+ * @date 2019骞�4鏈�16鏃�
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Good Morning");
+		System.out.println("update1");
+	}
+}
