240ec149e4c1b79ce9171291830591ef950645bf
710266074 <710266074@qq.com>
2019-04-17 08:52:01 +0800

    李四开发

--- src/main/java/com/nb/test/Hello.java
+++ src/main/java/com/nb/test/Hello.java
@@ -9,6 +9,6 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Good Morning");
 		System.out.println("update1");
-		System.out.println("ÀîËÄ¸üĞÂ.....");
+		System.out.println("æå››å¼€å‘");
 	}
 }
